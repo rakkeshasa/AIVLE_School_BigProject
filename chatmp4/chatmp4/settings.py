21298f2fb9a8c6c77469d7a6fe7c0a5c0f577246
Min Seok Kim <77041622+rakkeshasa@users.noreply.github.com>
2023-06-13 14:51:23 +0900

    new version upload

--- chatmp4/chatmp4/settings.py
+++ chatmp4/chatmp4/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "django_extensions",
+    "home"
 ]
 
 MIDDLEWARE = [
@@ -126,3 +128,5 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+AUTH_USER_MODEL = 'home.User'
