cd7231200221ffdc9efc27fcf4a1c987d5b44901
rakkeshasa <rakkeshasa2@gmail.com>
2023-06-20 09:08:13 +0900

    게시글 생성, 삭제 구현

--- chatmp4/chatmp4/urls.py
+++ chatmp4/chatmp4/urls.py
@@ -7,7 +7,9 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("", TemplateView.as_view(template_name='index.html'),name='index'),
     path("test", views.test),
-    path("post", views.signup),
+    path("post", views.login_view),
     path("blog/", views.post),
     path('blog/<int:pk>/', views.posting, name="posting"),
+    path('blog/new_post/', views.new_post),
+    path('blog/<int:pk>/remove', views.remove_post),
 ]
--- chatmp4/chatmp4/views.py
+++ chatmp4/chatmp4/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth import authenticate, login
 from django.shortcuts import render, redirect
+from django.utils import timezone
 from home.models import User, Post
 import json
 
@@ -65,3 +66,22 @@ def post(request):
 def posting(request, pk):
     post = Post.objects.get(pk=pk)
     return render(request, 'posting.html', {'post':post})
+
+@csrf_exempt  
+def new_post(request):
+    if(request.method == 'POST'):
+        post = Post()
+        if request.user.is_authenticated:
+            post.id2 = request.user
+        post.post_title = request.POST['postname']
+        post.post_text = request.POST['contents']
+        post.post_date = timezone.now()
+        post.save()
+    return render(request, 'new_post.html')
+
+def remove_post(request, pk):
+    post = Post.objects.get(pk=pk)
+    if request.method == 'POST':
+        post.delete()
+        return redirect('/blog/')
+    return render(request, 'remove_post.html', {'Post': post})
