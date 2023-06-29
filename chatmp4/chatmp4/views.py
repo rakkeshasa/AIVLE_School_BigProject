0cddba2756abd44746de53c0f0237ac586b80a33
hnOh <dhgksmf423@gmail.com>
2023-06-29 23:44:01 +0900

    user와 db 연결

--- chatmp4/chatmp4/views.py
+++ chatmp4/chatmp4/views.py
@@ -3,14 +3,15 @@ from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.shortcuts import render, redirect
 from django.utils import timezone
-from blog.models import Post, User
-# from django.contrib.auth.models import User
+from blog.models import Post, Video
+from django.contrib.auth.models import User
 import json
 import os
 from . import video_split_model
 from . import stt_model
 from . import chat_model
 from django.contrib.auth import authenticate, login
+from datetime import datetime
 
 
 def test(request) :
@@ -22,70 +23,29 @@ def post(request) :
     print(data['pwd'])
     return HttpResponse("login page")
 
-# @csrf_exempt
-# def login_view(request):
-#     data = json.loads(request.body)
-#     if User.objects.filter(username = data['id']).exists():
-#         print(User.objects.get(username = data['id']),data['id'])
-#         print(User.objects.get(password = data['pwd']),data['pwd'])
-#         user = authenticate(request, username=data['id'], password=data['pwd'])
-#         # user = data['id']
-#         print(user)
-#         getUser = User.objects.get(username = data['id'])
-#         if user is not None:
-#             login(request, username=data['id'], password=data['pwd'])
-#             request.session['login_user'] = data['id']
-#             return JsonResponse({'name': user.username, 'message': '로그인 성공'})
-#         else:
-#             context = {
-#                 "result": "아이디 또는 비밀번호가 올바르지 않습니다."
-#             }
-#             return JsonResponse({'error': 'wrong_idpw'}, status=400)
-#     else:
-#         context = {
-#             "result": "존재하지 않는 이메일입니다."
-#         }
-#         return JsonResponse({'error': '잘못된 요청입니다.'}, status=405)
-
+# 로그인
 @csrf_exempt
 def login_view(request):
     data = json.loads(request.body)
     if User.objects.filter(email = data['id']).exists():
-        # login(request, username=data['id'], password=data['pwd'])
-        # user = authenticate(request, username=data['id'], password=data['pwd'])
         getUser = User.objects.get(email = data['id'])
- 
+
         if getUser.password == data['pwd']:
-            request.session['user'] = data['id']
+            request.session['user'] = data['id'] #유저email
+            request.session['username'] = getUser.username # 작성자명
+            request.session['user_id'] = getUser.id # 유저ID
+            print(getUser.id)
             return JsonResponse({'status': True,
                                  'session_id': request.session.get('user')})
         else:
-            # request.session['loginOk'] = False
-            # context = {
-            #     "result": "아이디 또는 비밀번호가 올바르지 않습니다."
-            # }
-            # return JsonResponse({'error': 'wrong_idpw'}, status=400)
             return JsonResponse({'status': '아이디 또는 비밀번호가 올바르지 않습니다.'})
     else:
-    #     request.session['loginOk'] = False
-    #     context = {
-    #         "result": "존재하지 않는 id입니다."
-    #     }
-    # return JsonResponse({'error': '잘못된 요청입니다.'}, status=405)  
         return JsonResponse({'status': '존재하지 않는 id입니다.'})
     
 def logout(request):
     request.session.clear()
 
     return HttpResponse('logout')
-
-def test(request): 
-    user_id = request.session.get('user')
-    print(user_id)
-    if user_id : 
-        return HttpResponse('hello')
-    else :
-        return HttpResponse('ㅠㅠ')
     
 
 def mypage(request): 
@@ -97,38 +57,6 @@ def mypage(request):
     print(res)
     return JsonResponse(res)
 
-# @csrf_exempt
-# def login_view(request):
-#     data = json.loads(request.body)
-#     if User.objects.filter(email = data['id']).exists():
-#         username = User.objects.get(email=data['id'])
-#         password = data['pwd']
-#         user = authenticate(username=username, password=password)
-#         if user:
-#             if user.is_active:
-#                 auth_login(request, user)
-#                 return HttpResponseRedirect(request.GET.get('next',
-#                                             settings.LOGIN_REDIRECT_URL))
-#         else:
-#             error = 'Invalid username or password.'
-
-    # if User.objects.filter(email = data['id']).exists():
-    #     getUser = User.objects.get(email = data['id'])
-    #     if getUser.password == data['pwd']:
-    #         request.session['login_user'] = data['id']
-    #         return JsonResponse({'name': getUser.name, 'message': '로그인 성공'})
-    #     else:
-    #         request.session['loginOk'] = False
-    #         context = {
-    #             "result": "아이디 또는 비밀번호가 올바르지 않습니다."
-    #         }
-    #         return JsonResponse({'error': 'wrong_idpw'}, status=400)
-    # else:
-    #     request.session['loginOk'] = False
-    #     context = {
-    #         "result": "존재하지 않는 id입니다."
-    #     }
-    # return JsonResponse({'error': '잘못된 요청입니다.'}, status=405)  
 
 
 # 파일 업로드 및 학습
@@ -146,18 +74,23 @@ def videoUpload(request):
                 destination.write(chunk)
 
         print(uploadFile)
-        # id = request.POST['userId']
+        user_id = request.session.get('user_id')
+        video_title = request.POST['title']
+        user_email = request.session.get('user')
 
-        # video_title = request.POST['videoTitle']
-        # video_address = request.POST['videoAddress']
-        # upload_date = request.POST['uploadDate']
+        user = User.objects.get(email=user_email)
+        print(user_email)
+        print(user.id)
+
+        video = Video(id = user, video_title=video_title, video_addr=file_path, upload_date=datetime.now())
+        video.save()
 
         # 동영상 학습
         current_directory = os.path.dirname(os.path.abspath(__file__))
-        input_fath = os.path.join(current_directory,'test_file','video_file','input','ttt.mp4') # 비디오 경로 수정
-        output_fath = os.path.join(current_directory,'test_file','video_file','output')
-        tt = video_split_model.get_video_duration(input_fath)
-        video_split_model.split_video(input_fath, output_fath, tt, 30)
+        input_path = os.path.join(current_directory,'test_file','video_file','input','ttt.mp4') # 비디오 경로 수정
+        output_path = os.path.join(current_directory,'test_file','video_file','output')
+        tt = video_split_model.get_video_duration(input_path)
+        video_split_model.split_video(input_fath, output_path, tt, 30)
         print('split success')
 
         # stt
@@ -204,29 +137,6 @@ def video_split(request):
     print('split success')
     return HttpResponse("Split Success")
 
-def stt(request):
-    current_directory = os.path.dirname(os.path.abspath(__file__))
-    # input_path = os.path.join(current_directory,'test_file','video_file','output','split_1.mp4')
-    chatmp4_files = os.listdir(os.path.join(current_directory, 'test_file','video_file','output'))
-    for chatmp4_file in chatmp4_files:
-        input_path = os.path.join(current_directory,'test_file','video_file','output',chatmp4_file)
-        output_name = os.path.splitext(chatmp4_file)[0]
-        output_path = os.path.join(current_directory,'test_file','text_file','result',f'{output_name}.txt')
-   
-        stt_model.STT(input_path,output_path)
-    print('stt success')
-    return HttpResponse("STT Success")
-
-def chat(request):
-    current_directory = os.path.dirname(os.path.abspath(__file__))
-    txt_path = os.path.join(current_directory, 'test_file','text_file','result')
-    res = chat_model.chat("", isfirst=True, input_dir=txt_path, vectordb_dir=os.path.join(current_directory, 'db'), n=2, message='발화자는 무슨 이야기 중이야?')
-    print('chat success')
-    return HttpResponse(res)
-
-# @csrf_exempt
-# def
-
 @csrf_exempt
 def video2chat(request):
     #응답 받기
@@ -235,54 +145,11 @@ def video2chat(request):
 
     # video split
     current_directory = os.path.dirname(os.path.abspath(__file__))
-    # input_fath = os.path.join(current_directory,'test_file','video_file','input','ttt.mp4') # 비디오 경로 수정
-    # output_fath = os.path.join(current_directory,'test_file','video_file','output')
-    # tt = video_split_model.get_video_duration(input_fath)
-    # video_split_model.split_video(input_fath, output_fath, tt, 30)
-    # print('split success')
 
-    # # stt
-    # chatmp4_files = os.listdir(os.path.join(current_directory, 'test_file','video_file','output'))
-    # for chatmp4_file in chatmp4_files:
-    #     input_path = os.path.join(current_directory,'test_file','video_file','output',chatmp4_file)
-    #     output_name = os.path.splitext(chatmp4_file)[0]
-    #     output_path = os.path.join(current_directory,'test_file','text_file','result',f'{output_name}.txt')
-   
-    #     stt_model.STT(input_path,output_path)
-    # print('stt success')  
 
     q = str(data['question'])
     txt_path = os.path.join(current_directory, 'test_file','text_file','result')
-    res = chat_model.chat("", isfirst=True, input_dir=txt_path, vectordb_dir=os.path.join(current_directory, 'db'), n=1, message=q)
+    res = chat_model.chat("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", isfirst=True, input_dir=txt_path, vectordb_dir=os.path.join(current_directory, 'db'), n=1, message=q)
     print('chat success')
     # return JsonResponse({'result': res, 'message': '답변 성공'})
     return HttpResponse(res)
-
-# def post(request):
-#     postlist = Post.objects.all()
-#     return render(request, 'blog.html', {'postlist': postlist})
-
-# def posting(request, pk):
-#     post = Post.objects.get(pk=pk)
-#     return render(request, 'posting.html', {'post':post})
-
-# @csrf_exempt  
-# def new_post(request):
-#     if(request.method == 'POST'):
-#         post = Post()
-#         if request.user.is_authenticated:
-#             post.id2 = request.user
-
-#         post.post_title = request.POST['postname']
-#         post.post_text = request.POST['contents']
-#         post.post_date = timezone.now()
-#         post.save()
-
-#     return render(request, 'new_post.html')
-
-# def remove_post(request, pk):
-#     post = Post.objects.get(pk=pk)
-#     if request.method == 'POST':
-#         post.delete()
-#         return redirect('/blog/')
-#     return render(request, 'remove_post.html', {'Post': post})
